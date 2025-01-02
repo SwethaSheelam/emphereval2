3f77da1d9bad083c1a4a16a8e217c3568d1eea9d
sheelamswetha <sheelamswetha5@gmail.com>
2025-01-02 13:38:55 +0530

    CRUD operations prepared

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,18 @@
-let form=document.getElementById("form")
- form.addEventListener("submit",function(){
+let form = document.getElementById("form")
+
+form.addEventListener("submit", function(){
     event.preventDefault()
-    let email=form.email.value;
-    let password=form.password.value;
+    let email = form.email.value;
+    let password = form.password.value;
+
     console.log(email, password)
 
-    if(email=="empher@gmail.com" && password=="empher@123"){
-        alert("Login Success")
-        window.location.href="quiz.html"
-    }
+    // empher@gmail.com
+  // empher@123
+
+  if(email=="empher@gmail.com" && password=="empher@123"){
+    alert("Login Success, you are redirecting to quiz page")
+    window.location.href = "quiz.html"
+  }
+
 })
